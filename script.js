ce7c49f085731a3ec669dde74d17a0193c317d96
Gage <vadium3854@gmail.com>
2024-01-17 02:00:35 -0500

    The start of the calculator app

---
+++ script.js
@@ -0,0 +1,19 @@
+const add = (a, b) => {
+    result = a + b;
+    console.log(result);
+}
+
+const subtract = (a, b) => {
+    result = a - b;
+    console.log(result);
+}
+
+const multiply = (a, b) => {
+    result = a * b;
+    console.log(result);
+}
+
+const divide = (a, b) => {
+    result = a / b;
+    console.log(result);
+}
